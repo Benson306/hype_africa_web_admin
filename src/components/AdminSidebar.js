d60950681b4a1f4414b1008eb92acb9a1e571189
Benson Ndiwa <bnkimtai@gmail.com>
2023-11-06 02:43:02 +0300

    Admin Sidebar Done

--- src/components/AdminSidebar.js
+++ src/components/AdminSidebar.js
@@ -2,11 +2,11 @@ import React, { useContext, useState } from 'react'
 
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom'
-import CampaignIcon from '@mui/icons-material/Campaign';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import BrandingWatermarkIcon from '@mui/icons-material/BrandingWatermark';
-import PaidIcon from '@mui/icons-material/Paid';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { AdminAuthContext } from '../utils/AdminAuthContext';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 
 
 function AdminSidebar() {
@@ -27,12 +27,12 @@ function AdminSidebar() {
   return (
     <div
       className={`min-h-screen shadow-md  bg-gradient-to-l from-zinc-900 via-zinc-800 to-slate-800 ${
-        isOpen ? 'w-64' : 'w-16'
+        isOpen ? 'w-72' : 'w-16'
       } transition-width duration-500 ease-in-out fixed top-0 left-0 z-100 overflow-hidden`}
     >
         { isOpen && <Link onClick={handleSidebar} to={"/discover_campaigns"} >
             <h1 className='text-white text-2xl text-center mt-5'>HypeAfrica</h1>
-            <h2  className='text-gray-300 text-sm text-center mt-2'>Creator Dashboard</h2>
+            <h2  className='text-gray-300 text-sm text-center mt-2'>Web Admin Dashboard</h2>
             
             </Link> }
 
@@ -40,26 +40,32 @@ function AdminSidebar() {
 
         {/* <div className="mt-28" style={{marginLeft: isOpen ? '30px' : '10px', marginRight:'10px'}}> */}
         <div
-        className={`mt-28 ml-4 lg:ml-${isOpen ? '12' : '8'} `}
+        className={`mt-28 ml-4 lg:ml-${isOpen ? '12' : '5'} `}
       >
-            
 
-            <Link onClick={handleSidebar} className="flex text-white py-2 align-middle gap-4 hover:text-blue-400 mb-5" to={"/discover_campaigns"}>
-                <CampaignIcon />
-                <div style={{display: isOpen ? 'block' : 'none'}}>Discover Campaigns</div> 
+            <Link onClick={handleSidebar} className="flex text-white py-2 align-middle gap-4 hover:text-blue-400 mb-2" to={"/discover_campaigns"}>
+                <StickyNote2Icon sx={{fontSize: 28}}/>
+                <div style={{display: isOpen ? 'block' : 'none'}}>Manage Applications</div> 
+            </Link>
+
+            <Link onClick={() => handleSidebar()} className="flex text-white text-sm py-2 ml-8 lg:ml-8 mr-1 align-middle gap-4 hover:text-blue-200" to={"/view_content_applications"}>
+
+                    <div style={{display: isOpen ? 'block' : 'none'}}> + Content Creators' Applications</div> 
+
+            </Link>
+
+            <Link onClick={() => handleSidebar()} className="flex text-white text-sm py-2 ml-8 lg:ml-8 mr-1 align-middle gap-4 hover:text-blue-200 mb-2" to={"/view_influencer_applications"}>
+
+                    <div style={{display: isOpen ? 'block' : 'none'}}>+ Influencer Creators' Applications</div> 
+
             </Link>
             
 
             <Link onClick={handleSidebar}  className="flex text-white py-2 align-middle gap-4 hover:text-blue-400 mb-5" to={"/discover_campaigns"}>
-                <BrandingWatermarkIcon />
-                <div style={{display: isOpen ? 'block' : 'none'}}>Invoices</div>
+                <ManageAccountsIcon sx={{fontSize: 28}}/>
+                <div style={{display: isOpen ? 'block' : 'none'}}>Manage Admin Users</div>
             </Link>
             
-
-            <Link onClick={handleSidebar}  className="flex text-white py-2 align-middle gap-4 hover:text-blue-400 mb-5" to={"/discover_campaigns"}>
-                <PaidIcon />
-                <div style={{display: isOpen ? 'block' : 'none'}}>Wallet</div> 
-            </Link>
 
             <button onClick={(e)=> handleLogout(e)} className='absolute bottom-10 left-0 p-4 flex  align-middle gap-4 hover:text-red-700 bg-black text-blue-400 w-full justify-center place-items-center' >
               <LogoutIcon />
