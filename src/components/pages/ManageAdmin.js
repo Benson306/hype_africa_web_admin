729686a77d8be8272428fdbb668191552b95b35d
Benson Ndiwa <bnkimtai@gmail.com>
2023-12-08 09:57:43 +0300

    Modal

--- src/components/pages/ManageAdmin.js
+++ src/components/pages/ManageAdmin.js
@@ -13,6 +13,19 @@ function ManageAdmin() {
 
     const navigate = useNavigate();
 
+    const [modalOpen, setModalOpen] = useState(false);
+    const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
+  
+    const openModal = (index) => {
+      setCurrentMediaIndex(index);
+      setModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setModalOpen(false);
+    };
+  
+
     useEffect(()=>{
         fetch(`${process.env.REACT_APP_API_URL}/admin_users`)
         .then(response => response.json())
@@ -77,6 +90,8 @@ function ManageAdmin() {
       })
     }
 
+    const [selectedData, setSelectedData] = useState(null);
+
   return (
     <div className="w-full min-h-screen bg-neutral-300">
       <ToastContainer />
@@ -122,7 +137,7 @@ function ManageAdmin() {
             <td className="border border-neutral-500 py-2 px-4 w-1/6">{user.id}</td>
               <td className="border border-neutral-500 py-2 px-4 w-2/6">{user.email}</td>
               <td className="border border-neutral-500 py-2 px-4 w-1/6">
-                <button className='bg-blue-600 hover:bg-blue-900 shadow-lg text-white rounded-lg p-2'>
+                <button onClick={e =>{ e.preventDefault(); openModal(0); setSelectedData(user)}} className='bg-blue-600 hover:bg-blue-900 shadow-lg text-white rounded-lg p-2'>
                     Change Password
                 </button>
               </td>
@@ -136,6 +151,50 @@ function ManageAdmin() {
         </tbody>
         </table>
         </div>
+
+        {modalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-gray-800 bg-opacity-80">
+
+          <div className="bg-white p-5 rounded-lg shadow-lg lg:w-1/4">
+            <div className='flex justify-end mb-1 lg:mb-5'>
+                <button
+                    onClick={closeModal}
+                    className="text-white bg-red-500 hover:bg-red-600 px-2 py-1 rounded-full text-sm flex flex-end"
+                >
+                    X
+                </button>
+            </div>
+
+            <div className='w-full'>
+              <div className='text-2xl mb-2'>Change Password</div>
+              <hr />
+
+              { selectedData && <div className='mt-5'>
+                <label className='mb-2 font-bold'>Email</label>
+                <div className='mb-4'>{selectedData.email}</div>
+                </div>}
+
+                <form >
+                  <label className='font-bold'>Old Password</label>
+                  <br />
+                  <input type="password" className='p-2 mt-4 mb-4 border-2 rounded-lg w-full border-neutral-600 ' placeholder='Old Password' />
+                  <br />
+                  <label className='font-bold'>New Password</label>
+                  <br />
+                  <input type="password" className='p-2 mt-4 mb-4 border-2 rounded-lg w-full border-neutral-600 ' placeholder='New Password' />
+                  <br />
+                  <label className='font-bold'>Confirm New Password</label>
+                  <br />
+                  <input type="password" className='p-2 mt-4 mb-4 border-2 rounded-lg w-full border-neutral-600' placeholder='Confirm New Password' />
+                  <br />
+                  <button className='w-full p-2 bg-green-800 rounded-lg mt-2 text-white' >Save</button>
+                </form>
+            </div>
+
+    
+          </div>
+        </div>
+      )}
 
         
         </div>
